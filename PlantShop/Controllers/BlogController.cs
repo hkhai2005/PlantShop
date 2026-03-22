20f9b4fc90c52377a2db271afbaacb925f969df3
hkhai2005 <khainguyen090705@gmail.com>
2026-03-22 23:45:12 +0700

    blog,contact,about

---
+++ PlantShop/Controllers/BlogController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlantShop.Models;
+
+namespace PlantShop.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly PlantShopDbContext _context;
+        public BlogController(PlantShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var blogs = _context.TbBlogs.ToList();
+            return View(blogs);
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+        [Route("/blog/{alias}-{id}.html")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.TbBlogs == null)
+            {
+                return NotFound();
+            }
+
+            var blog = await _context.TbBlogs.FirstOrDefaultAsync(m => m.BlogId == id);
+
+            if (blog == null)
+            {
+                return NotFound();
+            }
+            ViewBag.blogComment = _context.TbBlogComments
+                .Where(i => i.BlogId == id)
+                .ToList();
+
+            return View(blog);
+        }
+    }
+}
--- PlantShop/Controllers/ContactController.cs
+++ PlantShop/Controllers/ContactController.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PlantShop.Models;
 
 namespace PlantShop.Controllers
 {
     public class ContactController : Controller
     {
+
+        private readonly PlantShopDbContext _context;
+        public ContactController(PlantShopDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(string name, string phone, string email, string message)
+        {
+            try
+            {
+                TbContact contact = new TbContact();
+                contact.Name = name;
+                contact.Phone = phone;
+                contact.Email = email;
+                contact.Message = message;
+                contact.CreatedDate = DateTime.Now;
+                _context.Add(contact);
+                _context.SaveChangesAsync();
+                return Json(new { status = true });
+            }
+            catch
+            {
+                return Json(new { status = false });
+            }
         }
     }
 }
